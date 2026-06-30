ae3ed9e9a33457e3f896ce166573f16434113c3d
Luiza Amaral <luamaral1702@gmail.com>
2026-06-30 18:29:24 -0300

    ok

---
+++ glitch.py
@@ -0,0 +1,22 @@
+import time
+
+num = int(input("Escolha um número de 4 caracteres aleatório: "))
+count = 0
+
+if len(str(num)) != 4:
+    print("O numero deve ter 4 caracteres")
+else:
+    while num != 6174:
+        count += 1
+
+        a = str(num).zfill(4)
+
+        crescente = int(''.join(sorted(a)))
+        decrescente = int(''.join(sorted(a, reverse=True)))
+
+        num = decrescente - crescente
+
+        print(decrescente, "-", crescente, "=", num)
+        time.sleep(0.5)
+
+print("Número de tentativas:", count)
